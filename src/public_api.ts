6db2cc24049288fbbf60c4ad23680a20f85038c1
Amit Yadav <154998+ayadav@users.noreply.github.com>
2020-04-13 00:31:56 +0530

    Changes

--- src/public_api.ts
+++ src/public_api.ts
@@ -8,4 +8,5 @@ export * from './lib/openvidu-angular.module';
 export * from 'openvidu-browser';
 export * from './lib/shared/layout/openvidu-layout';
 export { UserModel } from './lib/shared/models/user-model';
+export {  } from './lib/shared/models/webcomponent-config';
 export { OvSettings } from './lib/shared/models/ov-settings';
